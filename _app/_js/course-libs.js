e43bfe565b077dedb1b47a981bd962c0c0f4b92e
PavelRoR <pavelvp2182@yandex.ru>
2021-05-10 15:52:19 +0500

    Course page first edits

--- _app/_js/course-libs.js
+++ _app/_js/course-libs.js
@@ -1,4 +1,4 @@
 //@prepros-prepend jquery-2.1.1.min.js
-//*@prepros-prepend marquee.js*//
+//@prepros-prepend popup.js
 //@prepros-prepend flipclock.min.js
 //@prepros-prepend slick.min.js
--- js/course.js
+++ js/course.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     /* Якорь */
-    $("a:not('.button-cost'):not('.button-prep')").click(function (f) {
+    $("a:not('.button-cost'):not('.button-prep'):not('.text-rev-link')").click(function (f) {
         // $(".header-menu li a, a.button-mobile,a[href='#prices']").click(function (f) {
         f.preventDefault();
         var a = $(this).attr("href"),
@@ -35,40 +35,20 @@ $(document).ready(function () {
             mml3.removeClass('switched');
         }
     });
-/*
-    if ($(window).width() > 561) {
-        $('.marquee-question').marquee({
-            duration: 15000,
-            startVisible: true,
-            duplicated: true
-        });
-    }
-    */
-/* Таймер */
-$(function () {
-    var clock;
-    var date = new Date("May 12, 2021 00:00 UTC+3");
-    var currentDate = new Date();
+    /* Таймер */
+    $(function () {
+        var clock;
+        var date = new Date("May 12, 2021 00:00 UTC+3");
+        var currentDate = new Date();
 
-    var link1 = {
-        name: $('#link-1'),
-        linkText: new Array('https://shop.mv-centr.ru/?r=ordering/cart/as1&id=1058&clean=true&lg=ru')
-    };
-    var link2 = {
-        name: $('#link-2'),
-        linkText: new Array('https://shop.mv-centr.ru/?r=ordering/cart/as1&id=1061&clean=true&lg=ru')
-    };
-    var link3 = {
-        name: $('#link-3'),
-        linkText: new Array('https://shop.mv-centr.ru/?r=ordering/cart/as1&id=1064&clean=true&lg=ru')
-    };
-    var prepLink = {
-        name: $('#prepLink'),
-        linkText: new Array('https://shop.mv-centr.ru/?r=ordering/cart/as1&id=1067&clean=true&lg=ru')
-    };
-
-
-    // if (currentDate < date) {
+        var link1 = {
+            name: $('#link-1'),
+            linkText: new Array('https://shop.iomp.ru/?r=ordering/cart/as1&id=157&clean=true&lg=ru')
+        };
+        var link2 = {
+            name: $('#link-2'),
+            linkText: new Array('https://shop.iomp.ru/?r=ordering/cart/as1&id=160&clean=true&lg=ru')
+        };
         var futureDate = date;
         var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
         clock = $('.clock').FlipClock(diff, {
@@ -80,27 +60,20 @@ $(function () {
                     $('.timer, .cost-sale, .cost-today,.prepayment').remove();
                     link1.name.attr('href', link1.linkText[0]);
                     link2.name.attr('href', link2.linkText[0]);
-                    link3.name.attr('href', link3.linkText[0]);
-                    prepLink.name.attr('href', prepLink.linkText[0]);
                     $('.cost-full span').addClass('unbroken');
                 }
             }
         });
-    // }  else {
-    //     var futureDate = dates[1];
-    //     var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
-    //     clock = $('.clock').FlipClock(diff, {
-    //         clockFace: 'HourlyCounter',
-    //         countdown: true,
-    //         language: 'ru',
-    //         callbacks: {
-    //             stop: function () {
-    //                 $('.prepayment').remove();
-    //             }
-    //         }
-    //     });
-    // }
-});
+    });
+    /*Видео-отзывы*/
+    $(".video-wrapper-rev").click(function () {
+        $(".video-wrapper-rev iframe").each(function () {
+            var l = $(this).parent().attr('data-img');
+            $(this).parent().html('<img src="' + l + '" alt="Видео отзыв">');
+        })
+        var a = $(this).attr("data-youtube");
+        $(this).html('<iframe src="https://www.youtube.com/embed/' + a + '?showinfo=0&rel=0&autoplay=1&mute=1&modestbranding=1"  allowfullscreen></iframe>')
+    });
     // Слайдер с Тамарой
     $('.author-slides').slick({
         autoplay: true,
@@ -115,6 +88,37 @@ $(function () {
         centerMode: false,
         pauseOnHover: false,
         pauseOnFocus: false
+    });
+    $('.revs-slider-text').slick({
+        autoplay: false,
+        autoplaySpeed: 3000,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        speed: 300,
+        arrows: true,
+        centerPadding: '40px',
+        centerMode: true,
+        appendArrows: '.arrows',
+        responsive: [{
+            breakpoint: 767,
+            settings: {
+                slidesToShow: 3
+            },
+            breakpoint: 561,
+            settings: {
+                slidesToShow: 1
+            }
+        }]
+    });
+    $('.slick-dots').wrap('<div class="container container-revs">');
+    $('.text-rev-link').magnificPopup({
+        type: 'image',
+        mainClass: 'mfp-fade',
+        gallery: {
+            enabled: true
+        }
     });
     /*Конец документа*/
 });
